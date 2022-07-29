549f73325283d23dbaea1bc3d7c63f6fd65ee783
Victor Owiti <viowiti12@gmail.com>
2022-07-29 17:13:29 +0300

    fixed the .env bug

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const schema = require("./src/schema/schema");
 
 const connectDB = require("./src/config/db");
 //Connect to database
-connectDB();
+connectDB(process.env.MONGO);
 
 const app = express();
 
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+const connectDB = async (MONGO) => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
+    const connection = await mongoose.connect(MONGO);
     console.log(
       `Connected to database at ${connection.connection.host}`.cyan.underline
         .bold
     );
   } catch (error) {
     console.log(error.message);
+    console.log("HERE");
   }
 };
 
